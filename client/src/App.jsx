214f3d86b230d940ec3ac1ca4f54f4edaacd42a8
ketankumarjha2007 <ketankumarjha70@gmail.com>
2026-03-12 18:31:50 +0530

    connected database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Donate from "./pages/Donate";
 import Contact from "./pages/Contact";
-import Footer from "./component/Footer"
+import Footer from "./component/Footer";
+
 function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/donate" element={<Donate />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer/>
+
+      <Footer />
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 import paymentRoutes from "./routes/payment.js";
 import contactRoutes from "./routes/contact.js";
-
 dotenv.config({ path: "./server/.env" });
 
 const app = express();
@@ -12,13 +12,22 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/contact", contactRoutes);
 app.use("/api/payment", paymentRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Backend running ");
+  res.send("Backend running");
 });
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB Connected");
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+
+})
+.catch((err) => {
+  console.error("MongoDB connection error:", err);
 });
