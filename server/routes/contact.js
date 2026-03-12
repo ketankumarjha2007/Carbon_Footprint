73143fac0d0e2d39b37f098445d5228782566617
ketankumarjha2007 <ketankumarjha70@gmail.com>
2026-03-13 01:57:52 +0530

    fix nodemailer smtp port

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -11,8 +11,8 @@ router.post("/send", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
